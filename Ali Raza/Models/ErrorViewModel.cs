086c1cb1441b58f59b0426ae24d830cb138f74f5
twakkal laptops <twakkal laptops@SHAZIL>
2024-06-10 17:28:16 -0700

    Add project files.

---
+++ Ali Raza/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ali_Raza.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
